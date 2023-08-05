5e6ef05d1c7ba62e54ce99925947180587838c98
faranora21 <farahuorm21@gmail.com>
2023-08-05 10:56:22 +0800

    Initial commit

---
+++ src/books.js
@@ -0,0 +1,21 @@
+ export const books = [
+    {
+       title : 'The Summer of Broken Rules',
+       author : 'K. L. Walther',
+       img : './img/book-1.jpg',
+       id : 1,
+    },
+    {
+       title : 'Atomic Habits',
+       author : 'James Clear',
+       img : './img/book-2.jpg',
+       id : 2,
+    },
+    {
+        title : 'Tom Lake',
+        author : 'Ann Patchett',
+        img : './img/book-3.jpg',
+        id : 3,
+     },
+    ];
+
